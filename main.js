785cb997cb734be617c1f37af336c788474a9e2e
Jasiel Guillén <darkensses@gmail.com>
2024-12-02 00:06:54 -0600

    experimenting with vertix and position.array

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Set up audio context
 const audioContext = new AudioContext();
@@ -44,6 +45,10 @@ camera.position.set(0,0,20);
 const scene = new THREE.Scene();
 scene.add(camera);
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
 window.addEventListener('resize', () => {
   // Update sizes
   sizes.width = canvas.parentElement.clientWidth;
@@ -65,24 +70,67 @@ const geometry = new THREE.BoxGeometry(0.5,0.5,0.5);
 const material = new THREE.MeshBasicMaterial({color: 'red'});
 const count = 32;
 const instancedMesh = new THREE.InstancedMesh(geometry,material,count);
-
-
 scene.add(instancedMesh);
+
+
+const planeGeo = new THREE.PlaneGeometry(16,16,32,32)
+const planeMesh = new THREE.Mesh(planeGeo, new THREE.MeshBasicMaterial({color: 'blue', wireframe: true}));
+scene.add(planeMesh);
+
+const test_array = [
+  1.67, 1.89, 1.85, 1.63, 1.48, 1.39, 1.21, 1.28,
+  1.29, 1.32, 1.26, 1.00, 1.10, 1.13, 1.01, 0.91,
+  0.67, 0.85, 0.89, 0.77, 0.65, 0.54, 0.31, 0.00,
+  0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00
+]
+
+let copyPlane = planeGeo.attributes.position.array.slice();
+for(let i = 0; i<test_array.length; i++) {
+  copyPlane[i*3+2] = test_array[i]*0.5;
+}
+
+console.log(planeGeo.attributes.position.array);
+console.log(copyPlane)
 
 /**
  * Animate
  */
 const clock = new THREE.Clock();
 const matrixAnim = new THREE.Matrix4();
+let delta = 0;
+// test_array.forEach((ele, idx) => {
+//   const planeIndex = Math.floor((idx + delta) % planeGeo.attributes.position.array.length);
+//   planeGeo.attributes.position.array[planeIndex * 3 + 2] = ele;
+//   if(idx === 0) {
+//     console.log(planeIndex,planeGeo.attributes.position.array)
+//   }
+// })
+// planeGeo.attributes.position.needsUpdate = true;
+// planeGeo.computeVertexNormals();
+
+// Review:
+// https://github.com/bigmstone/terrain/blob/master/src/index.js
 function animate(){
   analyser.getByteFrequencyData(dataArray);
-  //console.log(dataArray)
 
   for(let i=0, pad=0.5; i<bufferLength; i++) {
     const y = 10*dataArray[i]/128.0
     matrixAnim.makeScale(1,y,1);
     matrixAnim.setPosition(i-(count/2) + pad,y/4,0);
     instancedMesh.setMatrixAt(i,matrixAnim);
+
+
+    test_array.forEach((ele, idx) => {
+      const planeIndex = Math.floor((idx + delta) % copyPlane.length);
+      planeGeo.attributes.position.array[planeIndex * 3 + 2] = ele;
+
+      if(idx === 0) {
+        console.log(planeIndex,planeGeo.attributes.position.array)
+      }
+    })
+    planeGeo.attributes.position.needsUpdate = true;
+    planeGeo.computeVertexNormals();
+    delta += 0.06;
   }
 
   instancedMesh.instanceMatrix.needsUpdate = true;
